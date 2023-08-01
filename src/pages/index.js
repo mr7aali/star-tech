871cc847e94ae0fa4ddc63b836ec0b0b9d0a652b
mr7aali <mr7aali@gmail.com>
2023-08-01 23:53:46 +0600

    add server link

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,24 +6,29 @@ import RootLayouts from '../components/Layouts/RootLayouts'
 
 export default function HomePage({ data }) {
   // console.log(data);
+
+ 
+
   return (
     <div className='max-w-5xl grid grid-cols-3 gap-2 mx-auto my-24'>
       {
-        data?.map(c => <ProductCard key={c.id} data={c}/>)
+        data?.map(c => <ProductCard key={c.id} data={c} />)
       }
     </div>
   )
 }
+
+
 export const getStaticProps = async () => {
 
-  const res = await fetch("http://localhost:500/data");
+  const res = await fetch("https://test-server-coral-kappa.vercel.app/getdata");
   const data = await res.json();
-  // console.log(await data);
   return {
     props: {
       data: data
     }
   }
+
 }
 
 HomePage.getLayout = function getLayout(page) {
