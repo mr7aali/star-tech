891bfb89638cd8dae89a83996383e46209a311d5
mr7aali <mr7aali@gmail.com>
2023-10-26 01:54:03 +0600

    added responsive in banner header

--- src/components/HomePage/Banner.js
+++ src/components/HomePage/Banner.js
@@ -11,47 +11,48 @@ const Marquee = dynamic(() => import('../ui/Marquee'), { ssr: false });
 const Banner = () => {
     return (
         <>
-            <div className="max-w-[1290px] mx-auto gap-5  grid grid-cols-12 my-8">
-                <div className="col-span-9">
+            <div className="max-w-[1290px] lg:mx-auto gap-5  grid grid-cols-1 lg:grid-cols-12 my-8 mx-2">
+                <div className="lg:col-span-9">
 
                     <SwiperBanner  />
 
                 </div>
-                <div className=" gap-5 col-auto grid grid-rows-2 col-start-10 col-end-13">
+
+                <div className="gap-5 col-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 lg:grid-rows-2 lg:col-start-10 lg:col-end-13">
 
 
 
-                    <div className="bg-[#ffe8a1]  px-5">
-                        <h3 className="text-[14px] text-center font-semibold mt-5 ">Compare Products</h3>
+                    <div  className="bg-[#ffe8a1] px-2 xl:px-5">
+                        <h3 className="lg:text-[14px] text-[22px] text-center font-semibold mt-5 lg:mt-3 xl:mt-5">Compare Products</h3>
                         <small className="opacity-50 my-2 block text-center">Choose Two Products to Compare</small>
 
-                        <div className="my-3">
+                        <div  className="my-3">
                             <div className="relative">
-                                <input className="w-full my-2 outline-none py-2 pl-2 placeholder:text-[14px] before rounded-sm" placeholder="Search & Select Product" type="text" name="" id="" />
+                                <input className="w-full mt-2 lg:mt-1 outline-none py-2 pl-2 placeholder:text-[13px] xl:placeholder:text-[14px] before rounded-sm" placeholder="Search & Select Product" type="text" name="" id="" />
                                 <span className="absolute top-3 right-3 opacity-50"><SearchOutlined /></span>
                             </div>
 
                             <div className="relative">
-                                <input className="w-full my-2 outline-none py-2 pl-2 placeholder:text-[14px] before rounded-sm" placeholder="Search & Select Product" type="text" name="" id="" />
+                                <input className="w-full my-3 lg:my-2 outline-none py-2 pl-2 placeholder:text-[13px] xl:placeholder:text-[14px] before rounded-sm" placeholder="Search & Select Product" type="text" name="" id="" />
                                 <span className="absolute top-3 right-3 opacity-50"><SearchOutlined /></span>
                             </div>
 
-                            <div className="py-2">
-                                <button className="btn st-outline w-full ">View Comparison</button>
+                            <div className="xl:py-2">
+                                <button className="btn  st-outline w-full ">View Comparison</button>
                             </div>
                         </div>
 
                     </div>
                     <div className="bg-[black]">
 
-                        <Image width={300} height={193} className="w-[100%] h-[100%]" src="https://www.startech.com.bd/image/catalog/home/banner/small/budget-desktop-pc-27k-2.webp" alt="" />
+                        <Image width={300} height={190} className="w-[100%] h-[100%]" src="https://www.startech.com.bd/image/catalog/home/banner/small/budget-desktop-pc-27k-2.webp" alt="" />
                     </div>
                 </div>
                 
 
             </div>
 
-         \
+         
             <Marquee/>
         </>
     );
--- src/components/sheared/NavBar.js
+++ src/components/sheared/NavBar.js
@@ -3,12 +3,13 @@ import { HiOutlineInboxIn } from "react-icons/hi";
 import { AiOutlineSearch } from "react-icons/ai"
 import { MdOutlineElectricBolt, MdOutlineManageAccounts } from "react-icons/md";
 import Image from "next/image";
+import { BarsOutlined, SearchOutlined, ShoppingCartOutlined } from "@ant-design/icons";
 const NavBar = () => {
     const NavMenuList = ["Desktop", "Laptop", "Component", "Monitor", "UPS",
         "Phone", "Tablet", "Office Equipment", "Camera", "Security", "Networking",
         "Software", "Server & Storage", "Accessories", "Gadget", "Gaming", "TV", "AC"
     ].map(li => (
-        <li key={li} className="mx-2 py-4 text-[12px] font-serif font-bold cursor-pointer hover:text-[#ef4a23]">
+        <li key={li} className="xl:mx-2 mx-1 py-4 text-[10px] xl:text-[12px] xl:font-serif font-bold  cursor-pointer hover:text-[#ef4a23]">
             {li}
         </li>
     ))
@@ -16,25 +17,30 @@ const NavBar = () => {
     return (
         <div className="" style={{ boxShadow: '0px 1px 10px 0px rgba(0,0,0,0.31)', }}>
             <div className="top bg-[#081621] flex justify-center items-center">
-                <div className="mx-auto w-[1290px] flex items-center">
-                    <Link href={"/"} className="mx-4 py-3">
+                <div  className="mx-auto w-[1290px] flex items-center justify-between">
+                
+                    <div  className="pl-5 cursor-pointer">
+                        <span className="p-2 text-[#fff] text-[25px] flex justify-center items-center"><BarsOutlined /></span>
+                    </div>
+                
+                    <Link href={"/"} className="lg:mx-4 lg:py-3 py-2">
                         {/* <img className="w-[117px] " src="https://www.startech.com.bd/image/catalog/logo.png" alt="" srcSet="" /> */}
                         <Image
                             src="https://www.startech.com.bd/image/catalog/logo.png"
                             width={117}
-                            height={9}
+                            height={92}
                             alt=""
                         />
                     </Link>
 
-                    <div className=" h-[42px] flex-1 rounded-sm ml-5 relative">
+                    <div className="h-[42px] hidden lg:block flex-1 rounded-sm ml-5 relative">
                         <input className=" w-full rounded-sm h-full p-[15px] outline-none" placeholder="Search" type="text" />
 
                         <AiOutlineSearch className="absolute right-0 top-2 cursor-pointer  text-[black] mr-4 text-2xl" />
 
                     </div>
 
-                    <div  className="flex">
+                    <div  className="lg:flex hidden">
                         <div  className="ml-[15px] flex items-center cursor-pointer" >
                             <HiOutlineInboxIn className="text-[#ef4a23] mr-4 text-2xl" />
                             <div>
@@ -62,13 +68,18 @@ const NavBar = () => {
                             </div>
                         </div>
             
-                        <div className="ml-[10px]">
+                        <div className="ml-[10px] mr-3">
                             <Link href={"pc_builder"} className="btn">PC Builder</Link>
                         </div>
                     </div>
+
+                    <div className="flex mr-2">
+                        <span className="px-3 py-2 cursor-pointer text-[#fff] text-[25px] flex justify-center items-center"><SearchOutlined /></span>
+                        <span className="px-3 py-2 cursor-pointer text-[#fff] text-[25px] flex justify-center items-center"><ShoppingCartOutlined /></span>
+                    </div>
                 </div>
             </div>
-            <div className="flex justify-center items-center">
+            <div className="lg:flex justify-center items-center hidden">
                 <ul className="flex">
                     {NavMenuList}
                 </ul>
