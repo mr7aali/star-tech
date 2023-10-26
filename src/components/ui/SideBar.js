5757880174b72249c933229f85f3037bcd25b0a2
mr7aali <mr7aali@gmail.com>
2023-10-26 18:14:40 +0600

    added fremare motion

--- src/components/ui/SideBar.js
+++ src/components/ui/SideBar.js
@@ -1,48 +1,37 @@
-import { AiOutlineRollback } from "react-icons/ai"
-import { BiHomeSmile, BiUser } from "react-icons/bi"
-import {HiOutlineChatBubbleBottomCenterText} from "react-icons/hi"
-import {FiShoppingCart,FiSettings} from "react-icons/fi"
-import { useRef, useState } from "react"
-import {GiHamburgerMenu} from "react-icons/gi"
+import { useRef, useState } from 'react'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { AnimatePresence, motion } from 'framer-motion'
+import { useClickAway } from 'react-use'
+import { AiOutlineRollback } from 'react-icons/ai'
+import { BiHomeSmile, BiUser } from 'react-icons/bi'
+import { HiOutlineChatBubbleBottomCenterText } from 'react-icons/hi2'
+import { FiSettings, FiShoppingCart } from 'react-icons/fi'
 
+const Sidebar = ({open, setOpen}) => {
+//   const [] = useState(false)
+  const ref = useRef(null)
+  useClickAway(ref, () => setOpen(false))
+  const toggleSidebar = () => setOpen(prev => !prev)
 
-
-
-
-
-
-
-
-
-
-
-
-
-const Sidebar = ({open,setOpen}) => {
-    const items = [
-        { title: 'Home', Icon: BiHomeSmile, href: '#' },
-        { title: 'About', Icon: BiUser },
-        { title: 'Contact', Icon: HiOutlineChatBubbleBottomCenterText, href: '#' },
-        { title: 'Settings', Icon: FiSettings, href: '#' },
-        { title: 'Shop', Icon: FiShoppingCart, href: '#' },
-      ]
-    // const [open, setOpen] = useState(false)
-    const ref = useRef(null)
-    // useClickAway(ref, () => setOpen(false))
-    const toggleSidebar = () => setOpen(prev => !prev)
-  
-    return (
-      <>
-
-       
-
+  return (
+    <>
+      {/* <button
+        onClick={toggleSidebar}
+        className="p-3 border-2 border-zinc-800 rounded-xl"
+        aria-label="toggle sidebar"
+      >
+        <GiHamburgerMenu />
+      </button> */}
+      <AnimatePresence mode="wait" initial={false}>
         {open && (
           <>
-            <div
+            <motion.div
+              {...framerSidebarBackground}
               aria-hidden="true"
               className="fixed bottom-0 left-0 right-0 top-0 z-40 bg-[rgba(0,0,0,0.1)] backdrop-blur-sm"
-            ></div>
-            <div
+            ></motion.div>
+            <motion.div
+              {...framerSidebarPanel}
               className="fixed top-0 bottom-0 left-0 z-50 w-full h-screen max-w-xs border-r-2 border-zinc-800 bg-zinc-900"
               ref={ref}
               aria-label="Sidebar"
@@ -57,8 +46,7 @@ const Sidebar = ({open,setOpen}) => {
                   <AiOutlineRollback />
                 </button>
               </div>
-
-              {/* <ul>
+              <ul>
                 {items.map((item, idx) => {
                   const { title, href, Icon } = item
                   return (
@@ -68,22 +56,65 @@ const Sidebar = ({open,setOpen}) => {
                         href={href}
                         className="flex items-center justify-between gap-5 p-5 transition-all border-b-2 hover:bg-zinc-900 border-zinc-800"
                       >
-                        <span>{title}</span>
-                        <div>
+                        <motion.span {...framerText(idx)}>{title}</motion.span>
+                        <motion.div {...framerIcon}>
                           <Icon className="text-2xl" />
-                        </div>
+                        </motion.div>
                       </a>
                     </li>
                   )
                 })}
-              </ul> */}
-
-            </div>
+              </ul>
+            </motion.div>
           </>
         )}
-      </>
-    )
-  }
-  
+      </AnimatePresence>
+    </>
+  )
+}
 
-  export default Sidebar
+const items = [
+  { title: 'Home', Icon: BiHomeSmile, href: '#' },
+  { title: 'About', Icon: BiUser },
+  { title: 'Contact', Icon: HiOutlineChatBubbleBottomCenterText, href: '#' },
+  { title: 'Settings', Icon: FiSettings, href: '#' },
+  { title: 'Shop', Icon: FiShoppingCart, href: '#' },
+]
+
+const framerSidebarBackground = {
+  initial: { opacity: 0 },
+  animate: { opacity: 1 },
+  exit: { opacity: 0, transition: { delay: 0.2 } },
+  transition: { duration: 0.3 },
+}
+
+const framerSidebarPanel = {
+  initial: { x: '-100%' },
+  animate: { x: 0 },
+  exit: { x: '-100%' },
+  transition: { duration: 0.3 },
+}
+
+const framerText = delay => {
+  return {
+    initial: { opacity: 0, x: -50 },
+    animate: { opacity: 1, x: 0 },
+    transition: {
+      delay: 0.5 + delay / 10,
+    },
+  }
+}
+
+const framerIcon = {
+  initial: { scale: 0 },
+  animate: { scale: 1 },
+  transition: {
+    type: 'spring',
+    stiffness: 260,
+    damping: 20,
+    delay: 1.5,
+  },
+}
+
+
+export default Sidebar;
