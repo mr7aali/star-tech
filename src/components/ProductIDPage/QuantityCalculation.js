bdcf93c4d6e0a298cbb1e0461f97991fc537f9c3
mr7aali <mr7aali@gmail.com>
2023-12-01 00:02:01 +0600

    feature section done

--- src/components/ProductIDPage/QuantityCalculation.js
+++ src/components/ProductIDPage/QuantityCalculation.js
@@ -5,20 +5,23 @@ const QuantityCalculation = () => {
     const [count, setCount] = useState(0);
 
     return (
-        <div className="flex my-[40px] w-[138px] " style={{ border: '1px solid #edf2f5' }}>
-            <div onClick={() => setCount((c) => (count > 0) ? c - 1 : c)} className="w-full hover:bg-gray-100 cursor-pointer flex justify-center items-center">
-                <LuMinus />
+        <div className="grid grid-cols-2 items-center w-1/2">
+            <div className="flex my-[40px] w-[138px] " style={{ border: '1px solid #edf2f5' }}>
+                <div onClick={() => setCount((c) => (count > 0) ? c - 1 : c)} className="w-full hover:bg-gray-100 cursor-pointer flex justify-center items-center">
+                    <LuMinus />
+                </div>
+                <div
+                    className="w-full text-center py-2"
+                    style={{
+                        borderRight: '1px solid #edf2f5',
+                        borderLeft: '1px solid #edf2f5'
+                    }}
+                > {count}</div>
+                <div onClick={() => setCount((c) => c + 1)} className="w-full hover:bg-gray-100 cursor-pointer flex justify-center items-center">
+                    <LuPlus />
+                </div>
             </div>
-            <div
-                className="w-full text-center py-2"
-                style={{
-                    borderRight: '1px solid #edf2f5',
-                    borderLeft: '1px solid #edf2f5'
-                }}
-            > {count}</div>
-            <div onClick={() => setCount((c) => c + 1)} className="w-full hover:bg-gray-100 cursor-pointer flex justify-center items-center">
-                <LuPlus />
-            </div>
+            <button className="btn w-full">Buy Now</button>
         </div>
     );
 };
