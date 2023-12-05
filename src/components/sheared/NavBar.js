c4f9f6fcc487e843d1d6ee3e89a76e327f7b6e06
mr7aali <mr7aali@gmail.com>
2023-12-06 00:20:42 +0600

    added responsive navbar for mobile device

--- src/components/sheared/NavBar.js
+++ src/components/sheared/NavBar.js
@@ -1,9 +1,11 @@
 import Link from "next/link";
 import { HiOutlineInboxIn } from "react-icons/hi";
 import { AiOutlineSearch } from "react-icons/ai"
-import { MdOutlineElectricBolt, MdOutlineManageAccounts } from "react-icons/md";
+import { MdOutlineElectricBolt, MdOutlineManageAccounts, MdLocalOffer } from "react-icons/md";
 import Image from "next/image";
 import { BarsOutlined, SearchOutlined, ShoppingCartOutlined } from "@ant-design/icons";
+import { PiPlusMinusFill } from "react-icons/pi";
+import { RiBuilding3Line } from "react-icons/ri";
 
 import { useState } from "react";
 import dynamic from "next/dynamic";
@@ -22,8 +24,8 @@ const NavBar = () => {
 
     return (
         <header className="" >
-            <main className="relative" style={{ boxShadow: '0px 1px 10px 0px rgba(0,0,0,0.31)', }}>
-                <div className="top bg-[#081621] flex justify-center items-center">
+            <main  className="lg:relative fixed top-0 w-full z-10" style={{ boxShadow: '0px 1px 10px 0px rgba(0,0,0,0.31)', }}>
+                <div  className="top bg-[#081621] flex justify-center items-center">
                     <div className="mx-auto w-[1290px] flex items-center justify-between">
                         <div onClick={() => setOpen(!open)} className="pl-5 cursor-pointer lg:hidden">
                             <span className="p-2 text-[#fff] text-[25px] flex justify-center items-center"><BarsOutlined /></span>
@@ -39,14 +41,14 @@ const NavBar = () => {
                             />
                         </Link>
 
-                        <div className="h-[42px]  hidden lg:block flex-1 rounded-sm ml-5 relative">
+                        <div  className="h-[42px]  hidden lg:block flex-1 rounded-sm ml-5 relative">
                             <input className=" w-full rounded-sm h-full p-[15px] outline-none" placeholder="Search" type="text" />
 
                             <AiOutlineSearch className="absolute right-0 top-2 cursor-pointer  text-[black] mr-4 text-2xl" />
 
                         </div>
 
-                        <div className="lg:flex hidden">
+                        <div  className="lg:flex hidden">
                             <div className="ml-[15px] flex items-center cursor-pointer" >
                                 <HiOutlineInboxIn className="text-[#ef4a23] mr-4 text-2xl" />
                                 <div>
@@ -95,12 +97,27 @@ const NavBar = () => {
                     {NavMenuList}
                 </ul>
             </nav>
-            <footer className="h-[100px] z-[1] bg-[#081621] w-full fixed bottom-0">
-                <div>   <HiOutlineInboxIn className="text-[#ef4a23] mr-4 text-2xl" /></div>
-                <div> <MdOutlineElectricBolt className="text-[#ef4a23] mr-4 text-2xl" /></div>
-                <div> <MdOutlineManageAccounts className="text-[#ef4a23] mr-4 text-2xl" /></div>
-                <div></div>
-                <div></div>
+            <footer className="lg:hidden z-[1] bg-[#081621] w-full fixed bottom-0 flex justify-between px-10 py-3">
+                <div className="flex flex-col items-center justify-center">
+                    <MdLocalOffer className="text-white text-xl" />
+                    <small className="text-white text-[10px] pt-2 opacity-70">Offers</small>
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                    <MdOutlineElectricBolt className="text-white text-xl" />
+                    <small className="text-white text-[10px] pt-2 opacity-70">Winter Fest</small>
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                    <RiBuilding3Line className="text-white text-xl" />
+                    <small className="text-white text-[10px] pt-2 opacity-70">PC Builder</small>
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                    <PiPlusMinusFill className="text-white text-xl" />
+                    <small className="text-white text-[10px] pt-2 opacity-70">Compare (0)</small>
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                    <MdOutlineManageAccounts className="text-white text-xl" />
+                    <small className="text-white text-[10px] pt-2 opacity-70">Account</small>
+                </div>
             </footer>
         </header>
     );
