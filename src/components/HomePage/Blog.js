d618ae34b07fc60f810f5add148e5dc09c54530f
mr7aali <mr7aali@gmail.com>
2023-12-23 20:20:16 +0600

    a

--- src/components/HomePage/Blog.js
+++ src/components/HomePage/Blog.js
@@ -1,9 +1,33 @@
 /* eslint-disable react/no-unescaped-entities */
+import { HomePageBlogData } from "@/constant/data";
+import Link from "next/link";
+
 const Blog = () => {
     return (
         <div className='font-serif max-w-[1290px] mx-auto  px-2 mb-3'>
-            <h1 className='mb-5 mt-16 text-xl font-semibold'>Leading Computer, Laptop & Gaming PC Retail & Online Shop in Bangladesh</h1>
-            <p className='text-[#01132d]'>Technology has become a part of our daily lives, and we depend on tech products daily for a vast portion of our lives. There is hardly a home in Bangladesh without a tech product. This is where we come in. <span className='text-[#ef4a23]'>Star Tech Ltd.</span> started as a Tech Product Shop in March 2007. We focus on giving the best customer service in Bangladesh, following our motto of “Customer Comes First.” This is why Star Tech is the most trusted computer shop in Bangladesh today, capturing the loyalty of a large customer base. After a long 16-year journey, Star Tech Ltd. was certified with the renowned "ISO 9001:2015 certification" as a recognition for the best Quality Control Management System. As an ISO-certified organization, Star Tech Ltd. is now up to the international standards that specify a Quality Management System (QMS). This Certification denotes that the organization strictly maintains all sorts of regulatory requirements to provide customers with products and services of a global standard.</p>
+
+            <div>
+                <h1 className='mb-5 mt-5 text-xl font-semibold'>Best Laptop Shop in Bangladesh
+                </h1>
+                <p className='text-[#01132d]'>Star Tech is the most popular <Link href={"/"} className='hover:underline text-[#ef4a23]'>Laptop Brand Shop in BD.</Link> Star Tech <Link href={"/"} className='hover:underline text-[#ef4a23]'>Laptop</Link> Shop has the perfect device, whether you are a freelancer, officegoer, or student. Gamers love our collection of <Link href={"/"} className='hover:underline text-[#ef4a23]'>Gaming Laptops</Link> because we always bring the latest laptops in Bangladesh. As the best laptop shop in BD, a customer’s budget is our first concern. We bring the latest Intel Laptop and AMD Laptop under budget for every customer - from starters to expert users. Star Tech is considered the most trusted laptop shop in BD, allowing you to buy the best laptops from top laptop brands in the world. Along with the best laptop brands, our experts provide you with the best buying decisions based on your needs and budget - making Star Tech the most trusted laptop shop in Bangladesh. Star Tech lets you buy an official Apple <Link href={"/"} className='hover:underline text-[#ef4a23]'>MacBook</Link> Air or MacBook Pro from <Link href={"/"} className='hover:underline text-[#ef4a23]'>Apple Store in Bangladesh</Link> . Star Tech sells the latest models of the most popular laptop brands, such as - Razer, HP, Dell, Apple MacBook, Asus, Acer, Lenovo, Microsoft Surface, MSI, Gigabyte, i-Life, <Link href={"/"} className='hover:underline text-[#ef4a23]'>Walton</Link> , Xiaomi MI, Huawei, Chuwi, etc.</p>
+                <Link href={"/"} className='hover:underline text-[#ef4a23]'></Link>
+            </div>
+            <div>
+                <h1 className='mb-5 mt-5 text-xl font-semibold'>Best Desktop PC Shop In Bangladesh</h1>
+                <p className='text-[#01132d]'><Link href={"/"} className='hover:underline text-[#ef4a23]'>Star Tech</Link> has the most comprehensive array of <Link href={"/"} className='hover:underline text-[#ef4a23]'>Desktop PCs</Link>. We offer top-of-the-line Custom PC, <Link href={"/"} className='hover:underline text-[#ef4a23]'>Brand PC</Link>, All-in-One PC, and <Link href={"/"} className='hover:underline text-[#ef4a23]'>Portable Mini PC</Link> at Star Tech outlets, the best Desktop PC shop in Bangladesh, which are spread nationwide. Get your new iMac Desktop or Apple Mac Mini with an international warranty and servicing plan. You can always depend on the Star Tech PC shop experts to build the best desktop PC or computer with parts of your choice. Star Tech is Bangladesh's most reliable repair shop for PC, laptops, & other consumer electronics. Take your gaming or professional content creation to the next level with a large collection of high-end Gaming PC and Editing PC from Star Tech. You can build a complete personal computer with the best desktop PC parts picked by you with our <Link href={"/"} className='hover:underline text-[#ef4a23]'>PC Builder</Link> feature. The features let you <Link href={"/"} className='hover:underline text-[#ef4a23]'>pick PC parts</Link> to buy the best desktop PC anytime. Or, you can visit any Star Tech custom PC shop near you to build the best Desktop PC according to your taste, live, and in front of you.</p>
+                <Link href={"/"} className='hover:underline text-[#ef4a23]'></Link>
+            </div>
+
+            {/* {
+                HomePageBlogData.map((Item, i) => (
+                    <div key={i}>
+                        <h1 className='mb-5 mt-5 text-xl font-semibold'>{Item.title}</h1>
+                        <p className='text-[#01132d]'>{String(Item.details)}</p>
+                        <Link href={"/"} className='hover:underline text-[#ef4a23]'></Link>
+                    </div>
+                ))
+            } */}
+
         </div>
     );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,34 +1,25 @@
-
-import Blog from '@/components/HomePage/Blog';
 import RootLayouts from '../components/Layouts/RootLayouts'
 import Banner from '@/components/HomePage/Banner';
 import FeaturedCategory from '@/components/HomePage/FeaturedCategory';
 import FeaturedProducts from '@/components/HomePage/FeaturedProducts';
 import HeadTag from '@/components/sheared/HeaderTag';
+import MainBlog from '@/components/HomePage/MainBlog';
+import Blog from '@/components/HomePage/Blog';
 
 export default function HomePage({ data }) {
-
-
-
-
 
   return (
     < >
       <HeadTag descriptionContent={"Star Tech HomePage"} title={"Star Tech"} />
       <Banner />
       <FeaturedCategory />
-      <Blog />
-
-      {/* <FeaturedProducts products={data?.data} /> */}
-
-
-
+      <FeaturedProducts products={data?.data} />
+      <MainBlog />
+      <Blog/>
       <div className='mt-[100px]'></div>
     </>
   )
 }
-
-
 HomePage.getLayout = function getLayout(page) {
   return (
     <RootLayouts>
