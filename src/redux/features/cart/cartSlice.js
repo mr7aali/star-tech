5240f7f4f3878e1a5603d4d9fdc83919c878fc26
mr7aali <mr7aali@gmail.com>
2024-01-14 23:40:37 +0600

    addToCart done

--- src/redux/features/cart/cartSlice.js
+++ src/redux/features/cart/cartSlice.js
@@ -7,9 +7,47 @@ const cartSlice = createSlice({
     initialState,
     reducers: {
         addToCart: (state, action) => {
-            state.cart = action.payload
-            console.log(state);
+            if (state.cart.length === 0) {
+                state.cart.push(action.payload);
+            }
+            else if (state.cart.length > 0) {
+                const needToquantityChange = state.cart.find((item) => item.id === action.payload.id);
+
+                if (needToquantityChange) {
+                    state.cart = state.cart.filter((item) => item.id !== action.payload.id);
+                    needToquantityChange.quantity += action.payload.quantity;
+                    state.cart.push(needToquantityChange);
+                }
+                else {
+                    state.cart.push(action.payload)
+                }
+
+            }
+            // const getPreviousDataArray = getCartDataToLocalStorage(KeyCartData).cart;
+            // if (getPreviousDataArray.length > 0) {
+            //     const needToquantityChange = getPreviousDataArray.find((item) => item.id === productData.id);
+            //     if (needToquantityChange) {
+            //         const NotneedToquantityChange = getPreviousDataArray.filter((item) => item.id !== productData.id);
+            //         console.log(NotneedToquantityChange);
+            //         needToquantityChange.quantity += count;
+            //         NotneedToquantityChange.push(needToquantityChange);
+            //         setCartDataToLocalStorage(KeyCartData, { cart: NotneedToquantityChange })
+
+            //     }
+            //     else {
+            //         getPreviousDataArray.push(productData);
+            //         setCartDataToLocalStorage(KeyCartData, { cart: getPreviousDataArray })
+            //     }
+
+            // }
+            // else if (getPreviousDataArray.length === 0) {
+            //     setCartDataToLocalStorage(KeyCartData, { cart: [productData] })
+            // }
         }
+
+
+
+
     }
 });
 
